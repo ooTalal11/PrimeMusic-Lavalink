2765fd08a3dc54fa3806ae4150a03de7eca2a31c
ooTalal11 <tsash4657@gmail.com>
2025-03-15 09:08:43 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://ooTalal511:ooTalal@t2kmi.xmurr.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',

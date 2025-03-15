85943dd94e9e745908066467f967617b293e4dc1
ooTalal11 <tsash4657@gmail.com>
2025-03-15 11:06:05 +0300

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  activityName: "discord.gg/T2K", 
+  activityType: "Listening to",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
